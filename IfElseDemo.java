bcc2e69eb9c837998e9f9d015830912a27392039
paraggaykar <parag@tridentwebinfoservices.com>
2016-03-21 14:50:24 +0530

    Added files via upload

---
+++ IfElseDemo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author parag
+ */
+public class IfElseDemo {
+    public static void main(string[] arg)
+    {
+    int age =30;
+    
+     System.out.println("Age is"+age);      
+             }
+}
